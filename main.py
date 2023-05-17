af5bc1e0bc6458a7102d0465736a6a81ca9dd83a
xtronix2000 <47248272+xtronix2000@users.noreply.github.com>
2023-05-17 22:34:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import paramiko
 import socket
+import json
 from datetime import datetime
 
 
+ports = [0, 20, 21, 22, 23, 25, 53, 67, 68, 80, 88, 443, 161, 162, 3389, 7070]
+
+
 #  PENTEST
-def scan_port(ip, port):  # TCP SYN scanning
+def port_state(ip, port):  # TCP SYN scanning
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # создали TCP соединение
     client.settimeout(0.5)
     return not client.connect_ex((ip, port))  # возвращает 0 если операция прошла успешно
@@ -14,6 +18,7 @@ def read_banner(ip, port):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect_ex((ip, port))
     banner = client.recv(1024).decode('utf-8')
+    client.close()
     return banner
 
 
@@ -26,6 +31,15 @@ def get_config(host, login, password, command):
     config = stdout.read().strip().decode('ASCII')
     client.close()
     return config
-import paramiko
-import socket
-from datetime import datetime
+
+
+with open('hosts.json', 'r') as f:
+    hosts = json.load(f)
+
+"""for p in ports:
+    status = scan_port('192.168.202.82', p)
+    print(f'Port {p} is {status}')"""
+
+print(read_banner('192.168.202.82', 22))
+
+
