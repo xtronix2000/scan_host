6c0927f061124a611864b78c8b29beccdd48ee7b
xtronix2000 <47248272+xtronix2000@users.noreply.github.com>
2023-05-18 16:39:00 +0300

    Update core.py

--- core.py
+++ core.py
@@ -1,21 +1,30 @@
 import paramiko
 import socket
+import os
+import platform
+import pyping
 
 
-def scan_port(ip, port):  # TCP SYN scanning
+def ping(ip: str):
+    key = '-n' if platform.system().lower() == 'windows' else '-c'
+    nul = 'nul' if platform.system().lower() == 'windows' else '/dev/null'
+    return os.system(f'ping {key} 1 {ip} > {nul} 2>&1') == 0
+
+
+def scan_port(ip: str, port: int):  # TCP SYN scanning
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:  # создали TCP соединение
         client.settimeout(0.5)
         return not client.connect_ex((ip, port))
 
 
-def read_banner(ip, port):
+def read_banner(ip: str, port: int):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:  # создали TCP соединение
         client.connect_ex((ip, port))
         banner = client.recv(1024).decode('utf-8')
     return banner
 
 
-def get_config(host, login, password, command):
+def get_config(host: str, login: str, password: str, command: str):
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(host, port=22, username=login, password=password, timeout=3)
