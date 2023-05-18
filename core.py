d57e543c0bdb63fc6754c659f2be4f54d81e22c2
xtronix2000 <47248272+xtronix2000@users.noreply.github.com>
2023-05-18 16:54:14 +0300

    Update core.py

--- core.py
+++ core.py
@@ -2,7 +2,6 @@ import paramiko
 import socket
 import os
 import platform
-import pyping
 
 
 def ping(ip: str):
@@ -12,19 +11,19 @@ def ping(ip: str):
 
 
 def scan_port(ip: str, port: int):  # TCP SYN scanning
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:  # создали TCP соединение
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client: 
         client.settimeout(0.5)
         return not client.connect_ex((ip, port))
 
 
-def read_banner(ip: str, port: int):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:  # создали TCP соединение
+def read_banner(ip: str, port: int):  # few problems
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client: 
         client.connect_ex((ip, port))
         banner = client.recv(1024).decode('utf-8')
     return banner
 
 
-def get_config(host: str, login: str, password: str, command: str):
+def run_command(host: str, login: str, password: str, command: str):  # need to open 22 port
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(host, port=22, username=login, password=password, timeout=3)
